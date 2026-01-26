4192b8cd71bd74b8655707b5a59ae361bbccbe5a
Joe Ray Gregory <joe@may17.de>
2026-01-26 21:03:10 +0100

    Add solution

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import AppNavigation from '../components/AppNavigation.vue'
+import BookList from '../components/BookList.vue'
+</script>
+
+<template>
+  <main class="container">
+    <AppNavigation />
+    <BookList />
+  </main>
+</template>
